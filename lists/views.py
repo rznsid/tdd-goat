3f435ced1da9612436e2bccd7a9b90b85317470d
rznsid <simon.whittenbury@gmail.com>
2021-09-28 19:47:00 +1000

    FT failing with second list item

--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,8 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
 
 def home_page(request):
-    return render(request, "home.html")
+    return render(
+        request, "home.html", {"new_item_text": request.POST.get("item_text", "")}
+    )
